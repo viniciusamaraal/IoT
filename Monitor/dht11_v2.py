b97178efbb683df303b2bab26ab14c5444c2f4d6
Vinícius Amaral <viniciusluiz.doamaral@gmail.com>
2016-11-05 00:22:16 -0200

    Criacao de funcao para enviar notificacoes para os dispositivos que estiverem com o app instalado

--- Monitor/dht11_v2.py
+++ Monitor/dht11_v2.py
@@ -7,16 +7,41 @@ import time
 import datetime
 from decimal import *
 
-def definirAlarme(umidDec, tempDec):
+# Funcao responsavel por acionar o buzzer e o led caso os limites tenham sido ultrapassados
+def definirAlarme(tempDec, umidDec):
    tempExcedida = temp >= Decimal(config["valorAlertaTemperaturaMaxima"]) or temp <= Decimal(config["valorAlertaTemperaturaMinima"])
    umidExcedida = umid >= Decimal(config["valorAlertaUmidadeMaxima"]) or umid <= Decimal(config["valorAlertaUmidadeMinima"])
 
    if tempExcedida or umidExcedida:
      GPIO.output(pino_sensor_buzzer, GPIO.HIGH)
      GPIO.output(pino_sensor_led, GPIO.HIGH)
+
+     enviarNotificacao(tempExcedida, tempDec, umidExcedida, umidDec)
    else:
      GPIO.output(pino_sensor_buzzer, GPIO.LOW) 
      GPIO.output(pino_sensor_led, GPIO.LOW)
+
+# Funcao responsavel por chamar o servico que envia a notificacao para os dispositivos com o aplicativo instalado
+def enviarNotificacao(tempExcedida, tempDec, umidExcedida, umidDec):
+   dados = {}
+   dados["app_id"] = "11bfdd01-0741-4a4a-a105-3f2d3bfb79ad"
+   dados["included_segments"] = ["All"]
+
+   cabecalho = {}
+   cabecalho["Content-Type"] = "application/json"
+   cabecalho["Authorization"] = "Basic MTIwNGY4ZmUtZWU3OC00OTZmLTgzZTgtNGRhYmEwMWJkNjNk"
+
+   if tempExcedida:
+     mensagem = ("A temperatura ultrapassou o limite definido. Valor atual: {0:0.1f} C").format(tempDec)
+     dados["contents"] = { "en": mensagem }
+
+   if umidExcedida: 
+     mensagem = ("A umidade ultrapassou o limite definido. Valor atual: {0:0.1f}%").format(umidDec)
+     dados["contents"] = { "en": mensagem }
+
+   response = requests.post('https://onesignal.com/api/v1/notifications', headers = cabecalho, json = dados)
+   print ("Status de envio da notificacao: {0:0.1f}").format(response.status_code)
+
 
 # Define o tipo de sensor
 sensor = Adafruit_DHT.DHT22
@@ -37,7 +62,6 @@ GPIO.setup(pino_sensor_led, GPIO.OUT) # porta de saida do led
 print ("*** Lendo os valores de temperatura e umidade \n");
 
 
- 
 while(1):
    # Efetua a leitura do sensor
    umid, temp = Adafruit_DHT.read_retry(sensor, pino_sensor_temp_umid);
@@ -52,7 +76,7 @@ while(1):
      response = requests.post('https://projetoiotpuc.firebaseio.com/temperaturas.json', json = dados)
      
      print ("Temperatura = {0:0.1f}  Umidade = {1:0.1f}").format(temp, umid)
-     print ("Codigo de retorno = {0:0.1f}").format(response.status_code)
+     print ("Status de envio dos dados coletados: {0:0.1f}").format(response.status_code)
 
      config = requests.get('https://projetoiotpuc.firebaseio.com/config.json').json()
      
